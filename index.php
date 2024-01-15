90161f433d77c9bf8e98e187bc51801fedcec404
Suzanne G-C <suzanne.grelet@gmail.com>
2024-01-15 16:37:42 +0100

    Version finale

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 <body>
 
     <header>
+        <img src = "images/degradecouleur.png" id ="iconeTitre">
         <h1 id = "annonceAnniversaire">C'est mon anniversaire !</h1>
         <p>Moi, Suzanne Grelet--Cartier, ai l'immense honneur de vous inviter à ma fête d'anniversaire (20 ans, ça se fête, non ?)
         </p>
@@ -20,7 +21,7 @@
     <div id = "practialInformation">
         <div class="divPracticalInformation">
             <h1>Quand ?</h1>
-            <p>Le 3 mars à 15h30</p>
+            <p>Le 3 mars à 15h30 (c'est un dimanche...)</p>
         </div>  
         <div class="divPracticalInformation">
             <h1>Quel thème ?</h1>
@@ -33,18 +34,18 @@
             <a href="https://mairie-harcourt.fr/2019/07/30/location-salle-des-fetes-2/">Lien vers le site de la salle des fêtes</a>
         </div>
     </div>
-    <p id = "enSavoirPlus">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat neque quis arcu blandit maximus. Cras vel nisl tempus, porttitor mi sit amet, sodales odio. In risus ante, consequat ut pellentesque in, iaculis eu turpis. Morbi vehicula neque orci, at lacinia erat tincidunt sit amet. Nam nec sem vehicula, aliquam nulla at, scelerisque quam. Nunc aliquet justo sed ullamcorper aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris mattis nisi vitae risus sagittis, ut convallis mauris vulputate. Quisque nec sodales lorem, eget consectetur orci. Nam tristique interdum magna eu varius. Phasellus tincidunt, tortor sit amet tincidunt tempor, augue quam ullamcorper nibh, ut faucibus odio nulla in erat. </p>
+    <p id = "enSavoirPlus" style = "display:none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat neque quis arcu blandit maximus. Cras vel nisl tempus, porttitor mi sit amet, sodales odio. In risus ante, consequat ut pellentesque in, iaculis eu turpis. Morbi vehicula neque orci, at lacinia erat tincidunt sit amet. Nam nec sem vehicula, aliquam nulla at, scelerisque quam. Nunc aliquet justo sed ullamcorper aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris mattis nisi vitae risus sagittis, ut convallis mauris vulputate. Quisque nec sodales lorem, eget consectetur orci. Nam tristique interdum magna eu varius. Phasellus tincidunt, tortor sit amet tincidunt tempor, augue quam ullamcorper nibh, ut faucibus odio nulla in erat. </p>
 
     <img src="images/furet.png" id = "ferretIcon">
     <img src="images/salle-de-fêtes-extérieur.jpg" id = "placePicture">
 
     <div id = "inscriptions">
-        <form id = "birthdatForm" action = "register.php" method = "POST">
-            <input name = "nom" id = "nom">
-            <input name = "nombre" type = "number" id = "nombre"> 
+        <form id = "birthdayForm" action = "register.php" method = "POST">
+            <input name = "nom" id = "nom" placeholder = "Votre nom">
+            <input name = "nombre" type = "number" id = "nombre" placeholder = "Combien serez-vous ?"> 
             <button type = "submit" id = "sendInfo">Envoi</button>
         </form>
-        <h1>Combien d'inscrits ?</h1>
+        <h1>Déjà combien d'inscrits ?</h1>
         <p>Nombre total d'invités : <?php include "count_guests.php";?></p>
     </div>
 
